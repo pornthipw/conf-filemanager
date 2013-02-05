9e0d887f57816a0882d553b6f8700fbfd938b4aa
unknown <pornthip.wong@gmail.com>
2013-02-05 09:10:20 +0700

    add config

--- config.default.js
+++ config.default.js
@@ -14,6 +14,16 @@ module.exports = {
      poolSize: 4
     }
   },
+   upload: {
+    mongodb:{
+     host: 'localhost',
+     port: 27017,
+     db: 'conf_file',
+     //collection_name: 'nook_ac_1',
+     autoReconnect: true,
+     poolSize: 4
+    }
+  },
   site: {
     baseUrl: 'http://localhost:9011/',
     port: 9011,
